89c523745c09b185167c48955f1715f1729ac6db
Jason Fedler <Jason.Fedler@gmail.com>
2019-04-28 11:51:50 -0400

    Update DarkSky Service to use datetime stamp so that the timestamp is more obvious...and the epoch was wrong before.

--- Fms.Jpf.Submission.UI/weather-history/src/app/darksky.service.spec.ts
+++ Fms.Jpf.Submission.UI/weather-history/src/app/darksky.service.spec.ts
@@ -15,10 +15,22 @@ describe('DarkSkyService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should create a proper URL', () => {
+  it('should create a proper URL if time is provided', () => {
     const service: DarkSkyService = TestBed.get(DarkSkyService);
-    const url = service.getUrl('key', 15, 13, 69);
+    const url = service.getUrl('key', 15, 13, '2018-07-22');
     expect(url).toBe('https://cors-anywhere.herokuapp.com/' +
-                     'https://api.darksky.net/forecast/key/15,13,69?exclude=minutely,hourly,daily,alerts,flags');
+                     'https://api.darksky.net/forecast/key/15,13,2018-07-22?exclude=minutely,hourly,daily,alerts,flags');
+  });
+
+  it('should return a default timestamp of July 4, 2018 @ Noon if none is provided', () => {
+    const service: DarkSkyService = TestBed.get(DarkSkyService);
+    const url = service.useTimeProvidedOrDefaultToJuly42018AtNoon();
+    expect(url).toBe('2018-07-04T12:00:00');
+  });
+
+  it('should return the timestamp provided if provided', () => {
+    const service: DarkSkyService = TestBed.get(DarkSkyService);
+    const url = service.useTimeProvidedOrDefaultToJuly42018AtNoon('2018-08-04');
+    expect(url).toBe('2018-08-04');
   });
 });
--- Fms.Jpf.Submission.UI/weather-history/src/app/darksky.service.ts
+++ Fms.Jpf.Submission.UI/weather-history/src/app/darksky.service.ts
@@ -8,24 +8,29 @@ import { LocationItem } from './models/LocationItem';
 export class DarkSkyService {
   // include the exclude part to minimize the size of the returning payload
   urlMask = 'https://cors-anywhere.herokuapp.com/' + // Get around the CORS issue by using this lovely piece of code
-            'https://api.darksky.net/forecast/[KEY]/[LAT],[LONG],[EPOCH]?exclude=minutely,hourly,daily,alerts,flags';
-  epoch = 1530662400; // July 4, 2018 == 1530662400
+            'https://api.darksky.net/forecast/[KEY]/[LAT],[LONG],[TIME]?exclude=minutely,hourly,daily,alerts,flags';
+  july4AtNoon = '2018-07-04T12:00:00';
 
   // secret key - specific to user...
   secretKey = '[FILL_IN]';
 
   constructor(private http: HttpClient) { }
 
-  getWeatherDetails(locationItem: LocationItem) {
-    const url = this.getUrl(this.secretKey, locationItem.latitude, locationItem.longitude, this.epoch);
+  getWeatherDetails(locationItem: LocationItem, time?: string) {
+    const timeStamp = this.useTimeProvidedOrDefaultToJuly42018AtNoon(time);
+    const url = this.getUrl(this.secretKey, locationItem.latitude, locationItem.longitude, timeStamp);
     return this.http.get(url);
   }
 
-  getUrl(key: string, lat: number, long: number, time: number) {
+  useTimeProvidedOrDefaultToJuly42018AtNoon(time?: string) {
+    return time ? time : this.july4AtNoon;
+  }
+
+  getUrl(key: string, lat: number, long: number, time: string) {
     let url = this.urlMask.replace('[KEY]', key);
     url = url.replace('[LAT]', lat.toString());
     url = url.replace('[LONG]', long.toString());
-    url = url.replace('[EPOCH]', time.toString());
+    url = url.replace('[TIME]', time.toString());
     return url;
   }
 }
